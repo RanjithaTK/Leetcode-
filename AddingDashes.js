d41f3b787b1b955173aff97424a768bf2083ed18
im ranjitha <ranjithatk101@gmail.com>
2022-05-31 20:37:49 +0530

    changes

--- AddingDashes.js
+++ AddingDashes.js
@@ -1,5 +1,6 @@
-let nums = [3, 2, 2, 3]
+let nums = [3, 2, 2, 3, 2, 3]
 let value = 3
+
 function AddingDashes(nums, value) {
   let newNums = nums.map((num) => {
     if (num === value) {
@@ -7,17 +8,17 @@ function AddingDashes(nums, value) {
     }
     return num
   })
-
-  //   for (let i = 0; i < nums.length; i++) {
-  //     if (nums[i] === value) {
-  //       nums[i] = "-"
-  //     }
-  //   }
-  return newNums.push(newNums.splice(newNums.indexOf("-"), 1)[0])
-  //   array.push(array.splice(array.indexOf(element), 1)[0])
+  newNums.push(newNums.splice(newNums.indexOf("-"), 1)[0])
+  return newNums
 }
 console.log(AddingDashes(nums, value))
 
 // var arr = [3, 2, 2, 3]
 // arr.push(arr.splice(arr.indexOf(3), 1)[0])
 // console.log(arr)
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === value) {
+//       nums[i] = "-"
+//     }
+//   }
