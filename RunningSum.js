7bc960a7033461747bda02bc8bcc40aff40cb782
im ranjitha <ranjithatk101@gmail.com>
2022-06-01 12:13:08 +0530

    added another way

--- RunningSum.js
+++ RunningSum.js
@@ -11,10 +11,11 @@
 // console.log(RunningSum(nums))
 let nums = [1, 1, 1, 1, 1, 1]
 function RunningSum(nums) {
-  let ans = new Array(nums.length)
+  let ans = new Array(nums.length) // creates an empty array with length of 6 items
+  //assigns first item of array to be 1
   ans[0] = nums[0]
-  console.log(ans)
   for (let i = 1; i < nums.length; i++) ans[i] = ans[i - 1] + nums[i]
+  //adds up the previous item with current to return output array
   return ans
 }
 console.log(RunningSum(nums))
