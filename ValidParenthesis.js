87fc5cb025a9022ca6953d62715d495f8c1737fa
im ranjitha <ranjithatk101@gmail.com>
2022-06-04 12:30:37 +0530

    addede more comments

--- ValidParenthesis.js
+++ ValidParenthesis.js
@@ -19,9 +19,9 @@ const isValid = function (s) {
     //removes the lastmost element of array and now in the hand we have popped out element
     //compares each popped element with "}","]",")"
     if (s[i] !== requiredElement) return false
-    
+    //compares the popped element ie last element with that in the s[i]
   }
-
+  //we have to pop out all elements of the stack if anyone is left then length of stack !=0 and hence s has incmplt bracket
   if (stack.length > 0) return false
 
   return true
