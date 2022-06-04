2ae3a71d9bb54d068a81030297b95b31ba2b55e3
im ranjitha <ranjithatk101@gmail.com>
2022-06-04 12:12:19 +0530

    added comments

--- ValidParenthesis.js
+++ ValidParenthesis.js
@@ -9,13 +9,17 @@ const isValid = function (s) {
 
   for (let i = 0; i < s.length; i += 1) {
     if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
-      stack.push(closings[s[i]])
+      stack.push(closings[s[i]]) //identifies the closing tag corresponding to each opening ones in s and pushes to stack
+      //closings[s[i]] can b ")","}","]"
+      //   console.log(stack)
       continue
     }
 
     const requiredElement = stack.pop()
-
+    //removes the lastmost element of array and now in the hand we have popped out element
+    //compares each popped element with "}","]",")"
     if (s[i] !== requiredElement) return false
+    
   }
 
   if (stack.length > 0) return false
