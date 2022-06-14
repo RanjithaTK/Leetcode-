a9c9f36877e1c11ffb8f009d13fc646d913f10f1
im ranjitha <ranjithatk101@gmail.com>
2022-06-14 19:24:55 +0530

    changes

--- Remove Palindromic Subsequences.js
+++ Remove Palindromic Subsequences.js
@@ -6,3 +6,11 @@ function RemovePalindrome(S) {
   return 1
 }
 console.log(RemovePalindrome(S))
+// let S = "abbab"
+// function RemovePalindrome(S) {
+//   if (!S) return 0
+//   for (let i = 0, j = S.length - 1; i < j; i++, j--)
+//     if (S.charAt(i) !== S.charAt(j)) return 2
+//   return 1
+// }
+// console.log(RemovePalindrome(S))
