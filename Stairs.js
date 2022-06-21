64025ce0ac34d5707be1d4362cb942682fe92299
im ranjitha <ranjithatk101@gmail.com>
2022-06-21 17:28:06 +0530

    changes

---
+++ Stairs.js
@@ -0,0 +1,14 @@
+function staircase(n) {
+  // Write your code here
+  for (let i = 1; i <= n; i++) {
+    console.log("#".repeat(i).padStart(n))
+  }
+}
+staircase()
+// function staircase1(n) {
+//   // Write your code here
+//   for (let i = 1; i <= n; i++) {
+//     console.log("#".repeat(i))
+//   }
+// }
+// staircase1()
